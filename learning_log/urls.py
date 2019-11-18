50069592e8f6dc7761abfcdfc28ef94ad640a3d7
Mark Brown <allenmarkbrown@gmail.com>
2019-11-18 11:32:22 -0400

    thru p.411

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -1,9 +1,7 @@
-# imports module and function to manager URLs for the admin site
 from django.contrib import admin
 from django.urls import path, include
 
-# defines the urlpatterns variable
-urlpatterns = [ 
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('learning_logs.urls')),
 ]
